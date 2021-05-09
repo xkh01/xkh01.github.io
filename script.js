f61e9da07f146fd58352285759587c09da687b5c
xkh01 <xingkaihao0917@gmail.com>
2021-05-09 14:31:12 +0800

    Update Cookie program

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+window.onload=function(){
+	var user=getCookie('username');
+	if(user!="")alert('欢迎 '+user+' 再次访问');
+	else{
+		user=prompt('留个名字好吗，以便将后称呼您:');
+		if(user!=""&&user!=null)setCookie("username",user,36500);
+	}
+}
 function changeTitle(string){
 	document.title=string;
 }
 function backToHome(){
 	window.location.href='https://xkh01.tk';
+}
+function setCookie(name,value,exdays){
+	var exp=new Date();
+	exp.setTime(exp.getTime()+(exdays*86400000));
+	document.cookie=name+'='+value+';expires='+exp.toUTCString();
+}
+function getCookie(name){
+	var name=name+'=',ca=document.cookie.split(';');
+	for(var i=0;i<ca.length;i++){
+		var c=ca[i].trim();
+		if(c.indexOf(name)==0)return c.substring(name.length,c.length);
+	}
+	return"";
+}
+function delCookie(name){
+	setCookie(name,"",-1);
 }
