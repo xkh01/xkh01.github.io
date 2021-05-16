74b27c6a58644234e37898179e5730eec2dafb05
xkh01 <xingkaihao0917@gmail.com>
2021-05-16 13:58:30 +0800

    Update files

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var temp_n=0,temp_s='',username=getCookie('username');
+var arr=location.href.split('/'),fileName=arr[arr.length-1],temp_n=0,temp_s='',username=getCookie('username');
 window.onload=function(){
 	setCookie('visited','true',3650);
-	if(username!="")alert('欢迎 '+username+' 再次访问');
-	else{
-		if(getCookie('yuck')=='true');
+	if(fileName!='404.html'){
+		if(username!="")alert('欢迎 '+username+' 再次访问');
 		else{
-			username=prompt('留个名字好吗，以便将后称呼您:');
-			if(username==''||username==null)setCookie('yuck','true',3650);
+			if(getCookie('yuck')=='true');
 			else{
-				setCookie('username',username,3650);
-				setCookie('named','true',3650);
+				username=prompt('留个名字好吗，以便将后称呼您:');
+				if(username==''||username==null)setCookie('yuck','true',3650);
+				else{
+					setCookie('username',username,3650);
+					setCookie('named','true',3650);
+				}
 			}
 		}
-	}
-	if(getCookie('named')=='true'){
-		document.getElementById('welcome').innerHTML='Hi,'+username;
+		if(getCookie('named')=='true'){
+			document.getElementById('welcome').innerHTML='Hi,'+username;
+		}
 	}
 }
 function changeTitle(string){
@@ -24,7 +26,7 @@ function backToHome(){
 	window.location.href='/';
 }
 function modifyStance(id){
-	document.getElementById(id).innerHTML='按什么按';
+	document.getElementById(id).innerHTML='按啥？';
 }
 function setCookie(name,value,exdays){
 	var exp=new Date();
